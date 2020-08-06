f412e79018b330ed63c24b2969458f4bbe3ae641
Kwanpham <chomchomcapital@gmail.com>
2020-08-06 18:30:36 +0700

    update deploy scripts

--- cibuild.sh
+++ cibuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ssh -i deploy_rsa root@104.199.247.193 <<EOF
-  nohup java -jar /home/java/generate-password-hash-web-0.0.1-SNAPSHOT.jar &
+  nohup java -jar /home/java/generate-password-hash-web-0.0.1-SNAPSHOT.jar > /dev/null &
   echo "DONE"
 EOF
